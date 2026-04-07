aeae43f90f5747dcb38603886a6b451aa3dcdbdc
blin03 <82618800+blin03@users.noreply.github.com>
2026-04-07 16:12:43 -0400

    allow cross-origin requests

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
