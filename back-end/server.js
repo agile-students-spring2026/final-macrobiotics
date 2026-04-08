b7d7dc036d2f77705d0a47781150ec7361cd7772
blin03 <82618800+blin03@users.noreply.github.com>
2026-04-08 11:42:35 -0400

    add primitive endpoints with non-persistent store

--- back-end/server.js
+++ back-end/server.js
@@ -12,6 +12,25 @@ app.get("/", (req, res) => {
   res.send("API route reached successfully");
 });
 
+let bookmarks = [];
+
+//TODO: Add authenticated route param for users
+app.get("/api/bookmarks", (req, res) => {
+  res
+    .status(200)
+    .json({ message: "Bookmarks retrieved successfully", data: bookmarks });
+});
+
+app.post("/api/bookmarks", (req, res) => {
+  console.log("Bookmark request received:\n", {
+    flightNo: req.body.flightNo,
+    depAirport: req.body.depAirport,
+    arrAirport: req.body.arrAirport,
+  });
+  bookmarks.push(req.body);
+  res.status(201).json({ message: "Bookmark received", data: req.body });
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
